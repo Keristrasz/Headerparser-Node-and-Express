2037c11ef8de8c46fdcd5acb8be5b4227c9b3a44
Karel Ježík <jezikkarel@gmail.com>
2023-01-17 21:37:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// first API endpoint...
+
+// {"ipaddress":"78.102.13.66","language":"cs-CZ,cs;q=0.9,en;q=0.8","software":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
+
+app.get('/api/whoami', (req, res) => {
+  
+  console.log(req.headers);
+
+  res.json({ "ipaddress": req.get("x-forwarded-for"), "language": req.get("Accept-Language"), "software": req.get("User-Agent") });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
